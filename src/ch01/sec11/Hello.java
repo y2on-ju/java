b325f29c0ba66e7d3ce50a90c8be01976bf21b73
ghwj12 <123609582+y2on-ju@users.noreply.github.com>
2023-02-14 10:54:41 +0900

    first commit

---
+++ src/ch01/sec11/Hello.java
@@ -0,0 +1,24 @@
+package ch01.sec11;
+/**
+ * @author 여러분의 이름
+ */
+/*
+ 장제목: 1장 자바 시작하기
+ 작성일: 2023.02.14
+ */
+ 
+public class Hello {
+//프로그램 실행 진입점
+	 public static void main(String[] args) {
+		 //한 줄 주석
+		 /*
+		  * 
+		  * 여러 줄 주석
+		  */
+		 // 단축키: ctrl+/
+//		 여러줄하고 싶으면 여러줄 드래그 해서 단축키 하면됨
+		 
+		 //콘솔에 출력하는 실행문
+		 System.out.println("Hello, Java");
+	 }
+}
