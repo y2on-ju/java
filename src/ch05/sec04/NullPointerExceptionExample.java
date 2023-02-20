784e79f9e50ba91246ec8e2798e5feb692b2c51a
ghwj12 <123609582+y2on-ju@users.noreply.github.com>
2023-02-20 19:36:04 +0900

    java

---
+++ src/ch05/sec04/NullPointerExceptionExample.java
@@ -0,0 +1,12 @@
+package ch05.sec04;
+
+public class NullPointerExceptionExample {
+	public static void main(String[] args) {
+		int[] intArry = null;
+//		intArray[0] = 10;     //NullpointException
+		
+		String str = null;
+//		System.out.println("총 문자 수: " + str.length() );  //NullpointException
+	}
+
+}
