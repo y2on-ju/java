fc4839855a50cff269ca6043a57d077c0af9e859
ghwj12 <123609582+y2on-ju@users.noreply.github.com>
2023-03-13 17:48:57 +0900

    java

---
+++ src/javabasic/ch09/Ex9_2.java
@@ -0,0 +1,27 @@
+package javabasic.ch09;
+
+class Person {
+	long id;
+	
+	public boolean equals(Object obj) {
+		if(obj instanceof Person)
+			//obj가 Object타입이므로 id값을 참조하기 위해서는 Person타입으로 형변환 필요
+			return id ==((Person)obj).id; 
+		else
+			return false;  //타입이 Person이 아니면 값을 비교할 필요 없음
+	}
+	Person(long id) {
+		this.id = id;
+	}
+}
+class Ex9_2 {
+	public static void main(String[] args) {
+		Person p1 = new Person(8011081111222L);
+		Person p2 = new Person(8011081111222L);
+		
+		if(p1.equals(p2))
+			System.out.println("p1과 p2는 같은 사람입니다.");
+		else
+			System.out.println("p1과 p2는 다른 사람입니다.");
+	}
+}
---
+++ src/javabasic/ch09/Ex9_7.java
@@ -0,0 +1,12 @@
+package javabasic.ch09;
+
+class Ex9_7 {
+	public static void main(String[] args) {
+		String s1 = "AAA";
+		String s2 = "AAA";
+		String s3 = "AAA";
+		String s4 = "BBB";
+		
+	}
+
+}
