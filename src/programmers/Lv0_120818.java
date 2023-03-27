f61efd5b29a9af4ecaf2e3a8e54827094e3a0bce
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-03-27 17:19:30 +0900

    Lv0_120818

--- src/programmers/Lv0_120818.java
+++ src/programmers/Lv0_120818.java
@@ -1,5 +1,17 @@
 package programmers;
 
 public class Lv0_120818 {
-
+	class Solution {
+	    public int solution(int price) {
+	       int answer = 0;
+	        if(price<300000&&price>=100000){
+	            answer = (int)(price * 0.95f);
+	        }else if(price<500000&&price>=300000){
+	            answer =(int)(price * 0.9f);
+	        } else if(price>=500000){
+	            answer = (int)(price * 0.8f);
+	        }
+	        return answer;
+	    }
+	}
 }
