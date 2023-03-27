916d756096a00f51124ad9ae5bffa24d8eea3cf8
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-03-27 17:17:04 +0900

    Create Car.java

---
+++ src/ch12/sec11/exam02/Car.java
@@ -0,0 +1,30 @@
+package ch12.sec11.exam02;
+
+public class Car {
+	private String model;
+	private String owner;
+
+	public Car() {
+
+	}
+
+	public Car(String model) {
+		this.model = model;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+}
