63cb00176482ab52b9731b0e5c92805afff8c83a
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-03-27 17:17:11 +0900

    Create PrintAnnotation.java

---
+++ src/ch12/sec12/PrintAnnotation.java
@@ -0,0 +1,11 @@
+package ch12.sec12;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PrintAnnotation {
+	String value() default "-";
+	int number() default 15;
+}
+
